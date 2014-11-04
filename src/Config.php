1b83177ab84708dd48a7a139f23ba4f60e101f92
Remco Tolsma <info@remcotolsma.nl>
2014-11-04 13:15:49 +0100

    Classes from the Pronamic iDEAL plugin.

---
+++ src/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Title: ICEPAY config
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2014
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
+class Pronamic_WP_Pay_Gateways_Icepay_Config extends Pronamic_WP_Pay_GatewayConfig {
+	public $merchant_id;
+
+	public $secret_code;
+}
---
+++ src/ConfigFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Title: ICEPAY config factory
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2014
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
+class Pronamic_WP_Pay_Gateways_Icepay_ConfigFactory extends Pronamic_WP_Pay_GatewayConfigFactory {
+	public function get_config( $post_id ) {
+		$config = new Pronamic_Gateways_Icepay_Config();
+
+		$config->merchant_id = get_post_meta( $post_id, '_pronamic_gateway_icepay_merchant_id', true );
+		$config->secret_code = get_post_meta( $post_id, '_pronamic_gateway_icepay_secret_code', true );
+
+		return $config;
+	}
+}
