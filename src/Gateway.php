3cf737c8bad3eb074449ec43e6c151e6169fbb94
Reüel van der Steege <reuel@pronamic.nl>
2015-11-12 14:33:22 +0100

    Add support for payment methods

--- src/Gateway.php
+++ src/Gateway.php
@@ -72,9 +72,7 @@ class Pronamic_WP_Pay_Gateways_Icepay_Gateway extends Pronamic_WP_Pay_Gateway {
 	 * @return mixed
 	 */
 	public function get_issuer_field() {
-		$payment_method = $this->get_payment_method();
-
-		if ( Pronamic_WP_Pay_PaymentMethods::IDEAL === $payment_method ) {
+		if ( Pronamic_WP_Pay_PaymentMethods::IDEAL === $this->get_payment_method() ) {
 			return array(
 				'id'       => 'pronamic_ideal_issuer_id',
 				'name'     => 'pronamic_ideal_issuer_id',
@@ -84,6 +82,41 @@ class Pronamic_WP_Pay_Gateways_Icepay_Gateway extends Pronamic_WP_Pay_Gateway {
 				'choices'  => $this->get_transient_issuers(),
 			);
 		}
+	}
+
+	/////////////////////////////////////////////////
+
+	/**
+	 * Get payment methods
+	 *
+	 * @see Pronamic_WP_Pay_Gateway::get_payment_methods()
+	 */
+	public function get_payment_methods() {
+		return array( array(
+				'options' => array(
+					Pronamic_WP_Pay_PaymentMethods::IDEAL        => Pronamic_WP_Pay_PaymentMethods::IDEAL,
+					Pronamic_WP_Pay_PaymentMethods::CREDIT_CARD  => Pronamic_WP_Pay_PaymentMethods::CREDIT_CARD,
+					Pronamic_WP_Pay_PaymentMethods::DIRECT_DEBIT => Pronamic_WP_Pay_PaymentMethods::DIRECT_DEBIT,
+					Pronamic_WP_Pay_PaymentMethods::MISTER_CASH  => Pronamic_WP_Pay_PaymentMethods::MISTER_CASH,
+				)
+			)
+		);
+	}
+
+	/////////////////////////////////////////////////
+
+	/**
+	 * Get supported payment methods
+	 *
+	 * @see Pronamic_WP_Pay_Gateway::get_supported_payment_methods()
+	 */
+	public function get_supported_payment_methods() {
+		return array(
+			Pronamic_WP_Pay_PaymentMethods::IDEAL        => Pronamic_WP_Pay_PaymentMethods::IDEAL,
+			Pronamic_WP_Pay_PaymentMethods::CREDIT_CARD  => Pronamic_WP_Pay_PaymentMethods::CREDIT_CARD,
+			Pronamic_WP_Pay_PaymentMethods::DIRECT_DEBIT => Pronamic_WP_Pay_PaymentMethods::DIRECT_DEBIT,
+			Pronamic_WP_Pay_PaymentMethods::MISTER_CASH  => Pronamic_WP_Pay_PaymentMethods::MISTER_CASH,
+		);
 	}
 
 	/////////////////////////////////////////////////
