423bf82a235b630be8f4e97474ed270353ca85d5
Remco Tolsma <info@remcotolsma.nl>
2016-01-04 11:14:46 +0100

    Added tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,7 @@ module.exports = function( grunt ) {
 				],
 			},
 			options: {
+				bin: 'vendor/bin/phpcs',
 				standard: 'phpcs.ruleset.xml',
 				showSniffCodes: true
 			}
--- src/Gateway.php
+++ src/Gateway.php
@@ -98,8 +98,8 @@ class Pronamic_WP_Pay_Gateways_Icepay_Gateway extends Pronamic_WP_Pay_Gateway {
 					Pronamic_WP_Pay_PaymentMethods::CREDIT_CARD  => Pronamic_WP_Pay_PaymentMethods::CREDIT_CARD,
 					Pronamic_WP_Pay_PaymentMethods::DIRECT_DEBIT => Pronamic_WP_Pay_PaymentMethods::DIRECT_DEBIT,
 					Pronamic_WP_Pay_PaymentMethods::MISTER_CASH  => Pronamic_WP_Pay_PaymentMethods::MISTER_CASH,
-				)
-			)
+				),
+			),
 		);
 	}
 
