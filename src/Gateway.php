711c32429a11f282134e3b18a03d3c092859f1a4
Reüel van der Steege <reuel@pronamic.nl>
2018-10-23 12:21:28 +0200

    Use gateway constants for mode and method.

--- src/Gateway.php
+++ src/Gateway.php
@@ -38,7 +38,7 @@ class Gateway extends Core_Gateway {
 		parent::__construct( $config );
 
 		// Default properties for this gateway
-		$this->set_method( Gateway::METHOD_HTTP_REDIRECT );
+		$this->set_method( self::METHOD_HTTP_REDIRECT );
 		$this->set_slug( 'icepay' );
 	}
 
