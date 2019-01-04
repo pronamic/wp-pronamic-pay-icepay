9b90ca1dfea1b2539e29988a88a154a538559b9e
Reüel van der Steege <reuel@pronamic.nl>
2019-01-04 16:33:45 +0100

    Happy 2019.

--- src/Listener.php
+++ src/Listener.php
@@ -8,7 +8,7 @@ use Pronamic\WordPress\Pay\Plugin;
 /**
  * Title: ICEPAY listener
  * Description:
- * Copyright: Copyright (c) 2005 - 2018
+ * Copyright: 2005-2019 Pronamic
  * Company: Pronamic
  *
  * @author Remco Tolsma
