53e90d2506b57390040bca05499d1308ca2b24a7
Remco Tolsma <info@remcotolsma.nl>
2020-02-07 15:51:34 +0100

    Integration parent::__construct();.

--- src/Integration.php
+++ src/Integration.php
@@ -29,6 +29,8 @@ class Integration extends AbstractIntegration {
 			)
 		);
 
+		parent::__construct( $args );
+
 		$this->id            = $args['id'];
 		$this->name          = $args['name'];
 		$this->url           = $args['url'];
